cf8b5533b845dd65cb8fea55cf11f5d40924f4e6
rosebeck482 <becksh4267@gmail.com>
2025-06-13 08:19:22 -0400

    feat: configure for deployment and update project files

---
+++ graphrag-visualizer-main/src/app/components/GraphDataHandler.tsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import GraphViewer from "./GraphViewer";
+import { Box, Tab, Tabs } from "@mui/material";
+import useFileHandler from "../hooks/useFileHandler";
+import useGraphData from "../hooks/useGraphData";
+import DataTableContainer from "./DataTableContainer";
+import ReactGA from "react-ga4";
+
+const GraphDataHandler: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [tabIndex, setTabIndex] = useState(0);
+  const [graphType, setGraphType] = useState<"2d" | "3d">("2d");
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [selectedTable, setSelectedTable] = useState<
+    | "entities"
+    | "relationships"
+    | "documents"
+    | "textunits"
+    | "communities"
+    | "communityReports"
+    | "covariates"
+  >("entities");
+  const [includeDocuments, setIncludeDocuments] = useState(false);
+  const [includeTextUnits, setIncludeTextUnits] = useState(false);
+  const [includeCommunities, setIncludeCommunities] = useState(false);
+  const [includeCovariates, setIncludeCovariates] = useState(false);
+
+  const {
+    entities,
+    relationships,
+    documents,
+    textunits,
+    communities,
+    covariates,
+    communityReports,
+    loadDefaultFiles,
+  } = useFileHandler();
+
+  const graphData = useGraphData(
+    entities,
+    relationships,
+    documents,
+    textunits,
+    communities,
+    communityReports,
+    covariates,
+    includeDocuments,
+    includeTextUnits,
+    includeCommunities,
+    includeCovariates
+  );
+
+  const hasDocuments = documents.length > 0;
+  const hasTextUnits = textunits.length > 0;
+  const hasCommunities = communities.length > 0;
+  const hasCovariates = covariates.length > 0;
+
+  useEffect(() => {
+    loadDefaultFiles();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    const measurementId = process.env.REACT_APP_GA_MEASUREMENT_ID;
+    if (measurementId) {
+      ReactGA.initialize(measurementId);
+    } else {
+      console.error("Google Analytics measurement ID not found");
+    }
+  }, []);
+
+  useEffect(() => {
+    // **Set tab index based on the current path**
+    switch (location.pathname) {
+      case "/graph":
+        setTabIndex(0);
+        break;
+      case "/data":
+        setTabIndex(1);
+        break;
+      default:
+        setTabIndex(0);
+    }
+  }, [location.pathname]);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTabIndex(newValue);
+    let path = "/graph";
+    if (newValue === 1) path = "/data";
+    navigate(path);
+    ReactGA.send({
+      hitType: "event",
+      eventCategory: "Tabs",
+      eventAction: "click",
+      eventLabel: `Tab ${newValue}`,
+    });
+  };
+
+  const toggleGraphType = () => {
+    setGraphType((prevType) => (prevType === "2d" ? "3d" : "2d"));
+  };
+
+  const toggleFullscreen = () => {
+    setIsFullscreen(!isFullscreen);
+  };
+
+  return (
+    <>
+      <Tabs value={tabIndex} onChange={handleChange} centered>
+        <Tab label="Graph Visualization" />
+        <Tab label="Data Tables" />
+      </Tabs>
+      {tabIndex === 0 && (
+        <Box
+          p={3}
+          sx={{
+            height: isFullscreen ? "100vh" : "calc(100vh - 64px)",
+            width: isFullscreen ? "100vw" : "100%",
+            position: isFullscreen ? "fixed" : "relative",
+            top: 0,
+            left: 0,
+            zIndex: isFullscreen ? 1300 : "auto",
+            overflow: "hidden",
+          }}
+        >
+          <GraphViewer
+            data={graphData}
+            graphType={graphType}
+            isFullscreen={isFullscreen}
+            onToggleFullscreen={toggleFullscreen}
+            onToggleGraphType={toggleGraphType}
+            includeDocuments={includeDocuments}
+            includeTextUnits={includeTextUnits}
+            onIncludeDocumentsChange={() =>
+              setIncludeDocuments(!includeDocuments)
+            }
+            onIncludeTextUnitsChange={() =>
+              setIncludeTextUnits(!includeTextUnits)
+            }
+            includeCommunities={includeCommunities}
+            onIncludeCommunitiesChange={() =>
+              setIncludeCommunities(!includeCommunities)
+            }
+            includeCovariates={includeCovariates}
+            onIncludeCovariatesChange={() =>
+              setIncludeCovariates(!includeCovariates)
+            }
+            hasDocuments={hasDocuments}
+            hasTextUnits={hasTextUnits}
+            hasCommunities={hasCommunities}
+            hasCovariates={hasCovariates}
+          />
+        </Box>
+      )}
+
+      {tabIndex === 1 && (
+        <Box sx={{ display: "flex", height: "calc(100vh - 64px)" }}>
+          <DataTableContainer
+            selectedTable={selectedTable}
+            setSelectedTable={setSelectedTable}
+            entities={entities}
+            relationships={relationships}
+            documents={documents}
+            textunits={textunits}
+            communities={communities}
+            communityReports={communityReports}
+            covariates={covariates}
+          />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default GraphDataHandler;
